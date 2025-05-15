de1108290100d5b6eca779b8fd2d930c76963ee5
asher zeev aush <A0583251203@gmail.com>
2025-05-15 12:40:23 +0300

    A function that decodes a compass

--- atbash/Program.cs
+++ atbash/Program.cs
@@ -9,8 +9,38 @@ namespace atbash
     internal class Program
     {
         static void Main(string[] args)
+
         {
+            // A function that decodes the compass
+            void Encryption(string compass)
+            {
+                List<char> deciphered = new List<char>();
+                Dictionary<char, char> atbash = new Dictionary<char, char> ()
+                {
+                    {'A', 'Z'}, {'B', 'Y'}, {'C', 'X'}, {'D', 'W'}, {'E', 'V'},
+                    {'F', 'U'}, {'G', 'T'}, {'H', 'S'}, {'I', 'R'}, {'J', 'Q'},
+                    {'K', 'P'}, {'L', 'O'}, {'M', 'N'}, {'N', 'M'}, {'O', 'L'},
+                    {'P', 'K'}, {'Q', 'J'}, {'R', 'I'}, {'S', 'H'}, {'T', 'G'},
+                    {'U', 'F'}, {'V', 'E'}, {'W', 'D'}, {'X', 'C'}, {'Y', 'B'}, {'Z', 'A'},
+
+                    {'a', 'z'}, {'b', 'y'}, {'c', 'x'}, {'d', 'w'}, {'e', 'v'},
+                    {'f', 'u'}, {'g', 't'}, {'h', 's'}, {'i', 'r'}, {'j', 'q'},
+                    {'k', 'p'}, {'l', 'o'}, {'m', 'n'}, {'n', 'm'}, {'o', 'l'},
+                    {'p', 'k'}, {'q', 'j'}, {'r', 'i'}, {'s', 'h'}, {'t', 'g'},
+                    {'u', 'f'}, {'v', 'e'}, {'w', 'd'}, {'x', 'c'}, {'y', 'b'}, {'z', 'a'} {' ' , ' ' }
+                };
+                foreach (char c in compass)
+        {
+                    deciphered.Add(atbash[c]);
+                }
+                foreach (char c in deciphered)
+                {
+                    Console.Write(c);
+                }
+            }
           
+            Encryption("Gsv jfrxp yildm ulc qfhg");
+            Console.ReadLine();
         }
     }
 }
