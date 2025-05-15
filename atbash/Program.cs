4b9c3727b8ef97d195eea3ede01be05329fd6cdc
shlomo <shlomo2096@gmail.com>
2025-05-16 01:17:32 +0300

    add functhin Dangerous WordSearch

--- atbash/Program.cs
+++ atbash/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace atbash
 {
@@ -10,6 +11,32 @@ namespace atbash
     {
         static void Main(string[] args)
         {
+            //List<char> p = new List<char>() { 'h', 'l', 'o', 'w', ' ', 'b', 'o', 'm', 'b' };
+            //Console.WriteLine(DangerousWordSearch(p));
+            int DangerousWordSearch(List<char> DecodedList)
+            {
+                string DecryptedText = "";
+                int RiskScore = 0;
+                List<string> DangerousWordS = new List<string>() {"bomb", "nukhba", "fighter", "rocket", "secret"};
+                foreach (char l in DecodedList)
+                {
+                    DecryptedText += l;
+                }
+                string[] DecryptedArr = DecryptedText.Split(' ');
+                foreach (string str in DecryptedArr)
+                {
+                    foreach (string word in DangerousWordS)
+                    {
+                        if (str.ToLower() == word)
+                            RiskScore++;
+                    }
+                }
+                return RiskScore;
+
+
+
+            }
         }
+
     }
 }
